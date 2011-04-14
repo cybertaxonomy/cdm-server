5a33d7cf4d3426905f5f878a3469d43c72c13ce0
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-04-14 09:38:15 +0000

    minor

--- src/main/jenkins-ci/copy-cdmserver-and-restart.sh
+++ src/main/jenkins-ci/copy-cdmserver-and-restart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cp $WORKSPACE/../cdm-server/lastSuccessfulBuild/artifact/cdm-server/target/cdm-server-3.0.jar/copy-cdmserver-and-restart.sh /var/www /opt/cdmserver/cdm-server.jar.by-jenkins
+cp $WORKSPACE/../cdm-server/lastSuccessfulBuild/artifact/cdm-server/target/cdm-server-3.0.jar /var/www /opt/cdmserver/cdm-server.jar.by-jenkins
 /etc/init.d/cdmserver restart
