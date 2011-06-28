d511055d9e3ab3937c2d60729c6a4b13f3827faa
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-06-28 13:38:58 +0000

    working cdm-server deploy script for jenkins

--- src/main/jenkins-ci/copy-cdmserver-and-restart.sh
+++ src/main/jenkins-ci/copy-cdmserver-and-restart.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
+#
+# prior using this script make sure the requires sudo
+# commands are allowed in /etc/sudoers by allowing the
+# following command alias:
+#  Cmnd_Alias      CDMSERVER = /bin/cp -f cdm-server*.jar /opt/cdmserver/, /etc/init.d/cdmserver *, /bin/rm -f /opt/cdmserver/cdm-server*.jar, /usr/bin/ln -s cdm-server*.jar cdm-server.jar
 
-cp $WORKSPACE/../cdm-server/lastSuccessfulBuild/artifact/cdm-server/target/cdm-server-3.0.jar /var/www /opt/cdmserver/cdm-server.jar.by-jenkins
-/etc/init.d/cdmserver restart
+#
+
+MVN_PROJECT_TARGET_DIR="/home/andreas/workspaces/_svn-trunk/cdm-server/target"
+# MVN_PROJECT_TARGET_DIR="$WORKSPACE/../cdm-server/lastSuccessfulBuild/artifact/cdm-server/target"
+
+
+cd $MVN_PROJECT_TARGET_DIR
+
+CDMSERVER_JAR=$(ls -1 | grep cdm-server.*jar | grep -v sources)
+
+if [ ! -e $CDMSERVER_JAR ]
+then
+    echo "cdmserver*.jar missing in target folder"
+    exit 1
+fi
+
+sudo /etc/init.d/cdmserver stop
+sudo -u cdm rm /opt/cdmserver/cdm-server.jar
+sudo -u cdm cp -f $CDMSERVER_JAR /opt/cdmserver/
+cd $CDMSERVER_HOME
+sudo -u cdm ln -s $CDMSERVER_JAR cdm-server.jar
+sudo /etc/init.d/cdmserver start
