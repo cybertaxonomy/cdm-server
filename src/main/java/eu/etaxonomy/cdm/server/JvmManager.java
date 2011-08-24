857df1fc7eab2d6a539fe6234eb29db35eacabdb
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-08-24 10:56:15 +0000

    avoid uneccesary logging

--- src/main/java/eu/etaxonomy/cdm/server/JvmManager.java
+++ src/main/java/eu/etaxonomy/cdm/server/JvmManager.java
@@ -1,9 +1,9 @@
 // $Id$
 /**
 * Copyright (C) 2009 EDIT
-* European Distributed Institute of Taxonomy 
+* European Distributed Institute of Taxonomy
 * http://www.e-taxonomy.eu
-* 
+*
 * The contents of this file are subject to the Mozilla Public License Version 1.1
 * See LICENSE.TXT at the top of this package for the full license terms.
 */
@@ -23,42 +23,42 @@ import org.apache.log4j.Logger;
  *
  */
 public class JvmManager {
-	
-	/**
-	 * 
-	 */
-	private static final String SUFFIX_PERM_GEN = "Perm Gen";
-	
-	public static final Logger logger = Logger.getLogger(JvmManager.class);
-	
-	public static MemoryUsage getPermGenSpaceUsage(){
-		return getMemoryPoolUsage(SUFFIX_PERM_GEN);
-	}
 
-	protected static MemoryUsage getMemoryPoolUsage(String nameSuffix) {
-		List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
-    	
-    	for(MemoryPoolMXBean memoryPoolMXBean : memoryPoolMXBeans){
-			if(memoryPoolMXBean.getName().endsWith(nameSuffix)){
-    			logger.debug(memoryPoolMXBean.getName() 
-    					+ ": init= " + memoryPoolMXBean.getUsage().getInit()
-    					+ ", used= "+memoryPoolMXBean.getUsage().getUsed()
-    					+ ", max= "+memoryPoolMXBean.getUsage().getMax()
-    					+ ", committed= "+memoryPoolMXBean.getUsage().getCommitted());
-    			return memoryPoolMXBean.getUsage(); 
-    		}
-    	}
-    	return null;
-	}
-	
-	public static MemoryUsage getHeapMemoryUsage(){
-			
-			MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
-	    	if(memoryMXBean != null){
-	    		logger.info("NonHeapMemoryUsage: "+memoryMXBean.getHeapMemoryUsage());
-	    		return memoryMXBean.getHeapMemoryUsage();
-	    	}
-	    	return null;
-		}
+    /**
+     *
+     */
+    private static final String SUFFIX_PERM_GEN = "Perm Gen";
+
+    public static final Logger logger = Logger.getLogger(JvmManager.class);
+
+    public static MemoryUsage getPermGenSpaceUsage(){
+        return getMemoryPoolUsage(SUFFIX_PERM_GEN);
+    }
+
+    protected static MemoryUsage getMemoryPoolUsage(String nameSuffix) {
+        List<MemoryPoolMXBean> memoryPoolMXBeans = ManagementFactory.getMemoryPoolMXBeans();
+
+        for(MemoryPoolMXBean memoryPoolMXBean : memoryPoolMXBeans){
+            if(memoryPoolMXBean.getName().endsWith(nameSuffix)){
+                logger.debug(memoryPoolMXBean.getName()
+                        + ": init= " + memoryPoolMXBean.getUsage().getInit()
+                        + ", used= "+memoryPoolMXBean.getUsage().getUsed()
+                        + ", max= "+memoryPoolMXBean.getUsage().getMax()
+                        + ", committed= "+memoryPoolMXBean.getUsage().getCommitted());
+                return memoryPoolMXBean.getUsage();
+            }
+        }
+        return null;
+    }
+
+    public static MemoryUsage getHeapMemoryUsage(){
+
+            MemoryMXBean memoryMXBean = ManagementFactory.getMemoryMXBean();
+            if(memoryMXBean != null){
+                logger.debug("NonHeapMemoryUsage: "+memoryMXBean.getHeapMemoryUsage());
+                return memoryMXBean.getHeapMemoryUsage();
+            }
+            return null;
+        }
 
 }
