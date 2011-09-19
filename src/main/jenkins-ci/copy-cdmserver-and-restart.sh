1d10e6dbd23407c01d048015b0a149ccb83e4b6a
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2011-09-19 07:31:47 +0000

    fixing server replace/restart script

--- src/main/jenkins-ci/copy-cdmserver-and-restart.sh
+++ src/main/jenkins-ci/copy-cdmserver-and-restart.sh
@@ -3,7 +3,7 @@
 # prior using this script make sure the requires sudo
 # commands are allowed in /etc/sudoers by allowing the
 # following command alias:
-#  Cmnd_Alias      CDMSERVER = /bin/cp -f cdm-server*.jar /opt/cdmserver/, /etc/init.d/cdmserver *, /bin/rm -f /opt/cdmserver/cdm-server*.jar, /usr/bin/ln -s cdm-server*.jar cdm-server.jar
+#  Cmnd_Alias      CDMSERVER = /bin/cp -f cdm-server*.jar /opt/cdmserver/, /etc/init.d/cdmserver *, /bin/rm -f /opt/cdmserver/cdm-server.jar, /usr/bin/ln -s cdm-server*.jar cdm-server.jar
 
 #MVN_PROJECT_TARGET_DIR="/home/andreas/workspaces/_svn-trunk/cdm-server/target"
 MVN_PROJECT_TARGET_DIR="$WORKSPACE/cdm-server/target"
@@ -21,7 +21,7 @@ then
 fi
 
 sudo /etc/init.d/cdmserver stop
-sudo -u cdm rm /opt/cdmserver/cdm-server.jar
+sudo -u cdm rm -f /opt/cdmserver/cdm-server.jar
 sudo -u cdm cp -f $CDMSERVER_JAR /opt/cdmserver/
 cd $CDMSERVER_HOME
 sudo -u cdm ln -s $CDMSERVER_JAR cdm-server.jar
