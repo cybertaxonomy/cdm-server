8dddaadbcf5654bee8a389685ba836b99ade9b0b
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2014-04-01 11:58:05 +0000

    minor

--- src/main/java/eu/etaxonomy/cdm/server/instance/DataSourcePropertyParser.java
+++ src/main/java/eu/etaxonomy/cdm/server/instance/DataSourcePropertyParser.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import javax.xml.parsers.DocumentBuilder;
