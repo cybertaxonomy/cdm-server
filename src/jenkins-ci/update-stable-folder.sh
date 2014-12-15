a95d0ed3f65dce6c881b8ed013131ac51eb514ec
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2014-12-15 16:12:52 +0000

    adding note on deployment user

--- src/jenkins-ci/update-stable-folder.sh
+++ src/jenkins-ci/update-stable-folder.sh
@@ -2,7 +2,30 @@
 #
 # update the symlink for the stable folder
 #
+
+##############################################################
+# NOTE: the ssh host should be configured in the ~/.ssh/config:
+#     Host edit-deploy
+#     HostName <ip ot host name>
+#     User <deployment-user>
+# 
+# At the server to be delpoyed to you need to setup and configure the 
+
+# 1. crate the <deployment-user> 
+#
+# 2. assuming you are logged in as the <deployment-user>:
+#
+#  echo "umask 0126" >> ~/.bashrc : 
+#   
+# 3. user 'www-data' must be member of the group of the <deployment-user>
+#
+# 4. The permissions of the folders to delpoyed to must be set to 775 ownership must be adjusted:
+#   chmod -R 775 <the folders>
+#   chown -R www-data:deploy <the folders> 
+# 
+#  
 SSH_HOST='edit-deploy'
+##############################################################
 
 cd $WORKSPACE
 PROJECT_VERSION=(`cat target/classes/version.properties | grep "cdmlib-remote-webapp.version" | sed -e "s/[^0-9]*\([^\n\r]*\)/\1/g"`)
