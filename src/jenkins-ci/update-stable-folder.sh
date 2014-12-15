d9875e4cb2c10faffbf3a77b6daf685248bab931
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2014-12-15 14:16:16 +0000

    changing ssh host configuration

--- src/jenkins-ci/update-stable-folder.sh
+++ src/jenkins-ci/update-stable-folder.sh
@@ -2,13 +2,15 @@
 #
 # update the symlink for the stable folder
 #
+SSH_HOST='edit-deploy'
+
 cd $WORKSPACE
 PROJECT_VERSION=(`cat target/classes/version.properties | grep "cdmlib-remote-webapp.version" | sed -e "s/[^0-9]*\([^\n\r]*\)/\1/g"`)
 
 if [ -n "$PROJECT_VERSION" ]; then
- ssh root@wp5.e-taxonomy.eu "rm -r /var/www/download/cdmserver/stable"
- ssh root@wp5.e-taxonomy.eu "ln -s /var/www/download/cdmserver/$PROJECT_VERSION /var/www/download/cdmserver/stable"
- ssh root@wp5.e-taxonomy.eu "chown -R www-data:www-data /var/www/download/cdmserver/"$PROJECT_VERSION
- ssh root@wp5.e-taxonomy.eu "chown -R www-data:www-data /var/www/download/cdmserver/stable"
+ ssh $SSH_HOST "rm -r /var/www/download/cdmserver/stable"
+ ssh $SSH_HOST "ln -s /var/www/download/cdmserver/$PROJECT_VERSION /var/www/download/cdmserver/stable"
+ ssh $SSH_HOST "chown -R www-data:www-data /var/www/download/cdmserver/"$PROJECT_VERSION
+ ssh $SSH_HOST "chown -R www-data:www-data /var/www/download/cdmserver/stable"
 fi
 
