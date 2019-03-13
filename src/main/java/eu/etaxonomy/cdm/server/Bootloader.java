14a235fc524b45301160cd0f3fb329956b647b58
Andreas Kohlbecker <a.kohlbecker@bgbm.org>
2019-03-13 12:29:43 +0100

    ref #8173 setting spring.profiles.active in the cdm-sever bootloader if not defined otherwise

--- src/main/java/eu/etaxonomy/cdm/server/Bootloader.java
+++ src/main/java/eu/etaxonomy/cdm/server/Bootloader.java
@@ -78,6 +78,11 @@ public final class Bootloader {
     /**
      *
      */
+    private static final String SPRING_PROFILES_ACTIVE = "spring.profiles.active";
+
+    /**
+     *
+     */
     private static final String VERSION_PROPERTIES_FILE = "version.properties";
 
     //private static final String DEFAULT_WARFILE = "target/";
@@ -452,6 +457,11 @@ public final class Bootloader {
 
          // load the configured instances for the first time
         instanceManager.reLoadInstanceConfigurations();
+
+        if(System.getProperty(SPRING_PROFILES_ACTIVE) == null){
+            logger.info(SPRING_PROFILES_ACTIVE + " is undefined, and will be set to : \"remoting\"");
+            System.setProperty(SPRING_PROFILES_ACTIVE, "remoting");
+        }
 
         // in jetty 9 currently each connector uses
         // 2 threads -  1 to select for IO activity and 1 to accept new connections.
