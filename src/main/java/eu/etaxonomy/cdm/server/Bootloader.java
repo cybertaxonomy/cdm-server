e04ee17dfbac25b966797dd7f34b94516db5dec5
Andreas Müller <a.mueller@bgbm.org>
2026-06-20 01:02:30 +0200

    ref #10988, ref #10055, ref #6981 try to remove javax.servlet.jsp.jstl-. regex

--- src/main/java/eu/etaxonomy/cdm/server/Bootloader.java
+++ src/main/java/eu/etaxonomy/cdm/server/Bootloader.java
@@ -564,17 +564,6 @@ public final class Bootloader {
         setWebApp(defaultWebappContext, defaultWebAppFile);
 
         // JSP
-        //
-        // configuring jsp according to http://eclipse.org/jetty/documentation/current/configuring-jsp.html
-        // from example http://eclipse.org/jetty/documentation/current/embedded-examples.html#embedded-webapp-jsp
-        // Set the ContainerIncludeJarPattern so that jetty examines these
-        // container-path jars for tlds, web-fragments etc.
-        // If you omit the jar that contains the jstl .tlds, the jsp engine will
-        // scan for them instead.
-        defaultWebappContext.setAttribute(
-                "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern",
-                ".*/[^/]*servlet-api-[^/]*\\.jar$|.*/javax.servlet.jsp.jstl-.*\\.jar$|.*/[^/]*taglibs.*\\.jar$" );
-
         defaultWebappContext.addServletContainerInitializer(new JettyJasperInitializer());
         //for jetty 11+: defaultWebappContext.addEventListener(ContainerInitializer.asContextListener(new JettyJasperInitializer()));
 
